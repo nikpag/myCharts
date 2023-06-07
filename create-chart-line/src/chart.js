737a56c8e4bdd16f18f17df06249fb0585615694
Nikolaos Pagonas <el18175@mail.ntua.gr>
2023-06-07 14:28:05 +0300

    Chart: Fix CSV parser for line chart

--- create-chart-line/src/chart.js
+++ create-chart-line/src/chart.js
@@ -6,38 +6,36 @@ const csvFilePath = "line.csv";
 let i = 0;
 let chartData = {
 	title: "",
-	datasets: []
+	labels: [],
+	datasets: [],
 };
 
-fs.createReadStream(csvFilePath)
-	.pipe(csv.parse({ delimiter: "," }))
-	.on("data", (data) => {
-		if (i === 0) {
-			// console.log(i);
-			chartData.title = data[0];
-			// console.log(chartData);
-		}
-		else if (i == 1) {
-			// console.log(i);
-			for (let datasetLabel of data.slice(1)) {
-				chartData.datasets.push({
-					datasetLabel: datasetLabel,
-					data: []
-				});
+function csvToChartData() {
+	fs.createReadStream(csvFilePath)
+		.pipe(csv.parse({ delimiter: "," }))
+		.on("data", (data) => {
+			if (i === 0) {
+				chartData.title = data[0];
 			}
-			// console.log(chartData);
-		}
-		else {
-			// console.log(i);
-			for (let [j, v] of data.slice(1).entries()) {
-				chartData.datasets[j].data.push({ label: data[0], value: v });
+			else if (i == 1) {
+				for (let datasetLabel of data.slice(1)) {
+					chartData.datasets.push({
+						label: datasetLabel,
+						data: []
+					});
+				}
 			}
-			// console.log(chartData);
-		}
-		i++;
-	})
-	.on("end", () => {
-		for (item of chartData.datasets) {
-			console.log(item);
-		}
-	});
+			else {
+				for (let [j, v] of data.slice(1).entries()) {
+					chartData.datasets[j].data.push(v);
+				}
+				chartData.labels.push(data[0]);
+			}
+			i++;
+		})
+		.on("end", () => {
+			return chartData;
+		});
+}
+
+csvToChartData();
